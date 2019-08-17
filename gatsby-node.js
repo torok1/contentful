124f89ec35315faaff8d8d883f0de305ecba3ba9
torok1 <54214196+torok1@users.noreply.github.com>
2019-08-17 16:43:25 +0100

    Add files via upload

---
+++ gatsby-node.js
@@ -0,0 +1,43 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+const path = require("path")
+
+exports.createPage = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+  return new Promise((resolve, reject) => {
+    const blogPostTemplate = path.resolve("src/templates/blog-post.js")
+    resolve(
+      graphql(`
+        {
+          allContentfulBlog(limit: 100) {
+            edges {
+              node {
+                id
+                slug
+              }
+            }
+          }
+        }
+      `).then(result => {
+        if (result.errors) {
+          reject(result.errors)
+        }
+        result.data.allContentfulBlog.edges.forEach(edge => {
+          createPage({
+            path: edge.node.slug,
+            componenet: blogPostTemplate,
+            context: {
+              slug: edge.node.slug,
+            },
+          })
+        })
+        return
+      })
+    )
+  })
+}
